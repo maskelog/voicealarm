8ad5a2a3626aa927a28e271de246d0814bed450c
maskelog <blitzmaske@gmail.com>
2024-05-21 01:39:54 +0900

    퍼미션 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
 import 'providers/alarm_provider.dart';
 import 'screens/home_screen.dart';
-import 'utils/alarm_helper.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   await AndroidAlarmManager.initialize();
-  await AlarmHelper.initialize();
+  await _requestExactAlarmPermission();
+  await _initializeNotifications();
   runApp(const MyApp());
+}
+
+Future<void> _requestExactAlarmPermission() async {
+  if (await Permission.scheduleExactAlarm.isDenied) {
+    await Permission.scheduleExactAlarm.request();
+  }
+}
+
+Future<void> _initializeNotifications() async {
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('@drawable/app_icon');
+
+  const InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
 }
 
 class MyApp extends StatelessWidget {
@@ -18,7 +42,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (_) => AlarmProvider(),
+      create: (context) => AlarmProvider(),
       child: MaterialApp(
         title: 'Flutter Alarm App',
         theme: ThemeData(
